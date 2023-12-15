6222622e14d70a5c0606ecaa00cd5b2af0c5a59d
unknown <berktopbas@gmail.com>
2023-12-15 07:51:32 +0300

    Back&FrontInserter_v1

--- Back&FrontInserter.cpp
+++ Back&FrontInserter.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//For ease of printing container elements
 template<typename C>
 void print(const C& container, std::ostream& os = std::cout, const char* delimiter = " ")
 {
@@ -12,29 +13,38 @@ void print(const C& container, std::ostream& os = std::cout, const char* delimit
     cout << '\n';
 }
 
+// typical copy function
 template<typename InIter, typename OutIter> 
-OutIter Copy(InIter beg, InIter end, OutIter destbeg) // typical copy function
+OutIter Copy(InIter beg, InIter end, OutIter destbeg) 
 {
     while (beg != end)
         *destbeg++ = *beg++;
     return destbeg;
 }
 
+// possible implementation of back_insert_iterator
 template<typename C>
-class BackInsertIter // possible implementation of back_insert_iterator
+class BackInsertIter 
 {
 public:
-    BackInsertIter(C& c) : mc{ c } {} // initializes the reference by the ctor argument (container)
-    BackInsertIter& operator++() { return *this; } // eliminate the effects of dereference, postfix increment, and prefix increment by returning *this
-    BackInsertIter operator++(int) { return *this; }
-    BackInsertIter& operator*() { return *this; }
+    BackInsertIter(C& c) : mc{ c } {} // initialize the reference (C -- for Container )
+
+    /*
+    Eliminate the effects(aka making them no-op) of dereference, postfix increment, and prefix
+    increment by returning *this because modifying algorithms employ these operators in order 
+    to decide where to put values but back_insert_iterator only adds to the back, simply
+    push_back
+    */
+    BackInsertIter& operator++() { return *this; } 
+    BackInsertIter operator++(int) { return *this; } 
+    BackInsertIter& operator*() { return *this; }  
     BackInsertIter& operator=(typename const C::value_type& val) // copy and add the element to the back
     { 
         mc.push_back(val); 
         return *this;
     }
 private:
-    C& mc; // wraps the reference to the object which it receives as ctor argument (container)
+    C& mc; // wraps the reference to the object which it receives as ctor argument (Container)
 };
 
 template<typename C>
@@ -43,13 +53,14 @@ BackInsertIter<C> BackInserter(C& c) // possible implementation of back_inserter
     return BackInsertIter(c);
 }
 
+// possible implementation of front_insert_iterator
 template<typename C>
-class FrontInsertIter // possible implementation of front_insert_iterator
+class FrontInsertIter 
 {
 public:
-    FrontInsertIter(C& c) : mc{ c } {} // initializes the reference by the ctor argument (container)
-    FrontInsertIter& operator++() { return *this; } // eliminate the effects of dereference, postfix increment, and prefix increment by returning *this
-    FrontInsertIter operator++(int) { return *this; }
+    FrontInsertIter(C& c) : mc{ c } {}
+    FrontInsertIter& operator++() { return *this; } 
+    FrontInsertIter operator++(int) { return *this; } 
     FrontInsertIter& operator*() { return *this; }
     FrontInsertIter& operator=(typename const C::value_type& val) // copy and add the element to the front
     {
@@ -71,9 +82,11 @@ int main()
     vector<int> ivec1{ 1,2,3 }, ivec2, ivec3, ivec4;
     list<int> ilis1{ 10, 9, 8, 7 ,6 }, ilis2;
 
-    BackInsertIter iter1{ ivec1 }; // different ways of using BackInsertIterator class and BackInserter function
-    iter1 = 4;
-    BackInserter(ivec1) = 5;
+
+    // different ways of using BackInsertIterator class and BackInserter function
+    BackInsertIter iter1{ ivec1 };
+    iter1 = 4; // add 4 to back
+    BackInserter(ivec1) = 5; // add 5
 
     cout << "ivec1: ";
     print(ivec1);
